c2d65031d3d4b8a9a50cd8e18876a41be1f234b8
shivaanurgpatha-lang <shivaanuragpatha@gmail.com>
2026-07-09 23:48:35 +0530

    Split method is used

--- StringDivision.java
+++ StringDivision.java
@@ -1,4 +1,17 @@
 package com.java.Strings;
 
 public class StringDivision {
+    String str3;
+    String str4;
+    String str5;
+
+    static void main(String[] args) {
+        String names = "Shiva Kumar,Siddaramayya,SM Krishna, HD Deve gowda";
+        String[] cheifMinisters= names.split(",");
+        for (String cheifMinister : cheifMinisters) {
+            System.out.println(cheifMinister);
+        }
+    }
+
+
 }
